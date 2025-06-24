5e94626f21579dd6906f56e338bc289a2049a6ff
A-manH <amanhuss3n@gmail.com>
2025-06-24 17:35:07 -0400

    data being stored and displayed on new route registrants

--- proj1/app.py
+++ proj1/app.py
@@ -3,18 +3,28 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 SPORTS = ["Soccer", "Basketball", "Swimming", "Golf", "Tennis",]
+REGISTRANTS = {}
+
 
 @app.route("/")
 def index():
     return render_template("index.html", sports=SPORTS)
 
+
 @app.route("/register", methods=["POST"])
-def success():
-    if not request.form.get("name"):
+def register():
+    name = request.form.get("name")
+    if not name:
         return render_template("error.html", error="Input name field.")
 
+    sport = request.form.get("sport")
     if request.form.get("sport") not in SPORTS:
         return render_template("error.html", error=f'KeyError; key "{request.form.get("sport")}" is not an associated sport')
+    
+    REGISTRANTS[name] = sport
+    print(REGISTRANTS)
     return render_template("success.html")
 
-
+@app.route("/registrants")
+def view_registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
---
+++ proj1/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+
+d = {
+    "name": "macy",
+    "john": "soccer",
+    "alice": "tennis",
+    "bob": "golf",
+    "susan": "basketball",
+    "mike": "swimming"
+}
+for name in d:
+    print(name)
